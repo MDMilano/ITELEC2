d24833eb7953c8422414487fae3b6dba1ee5e2d2
MDMilano <marcdanielmilano@gmail.com>
2025-05-30 00:02:02 +0800

    separate the sign in and sign up page

--- signup.php
+++ signup.php
@@ -23,11 +23,16 @@
             <div class="col-12 col-md-6 col-lg-5 col-xl-4">
                 <div class="card border-0 shadow-sm">
                     <div class="card-header bg-white py-3">
-                        <h4 class="card-title text-center mb-0">SIGN IN</h4>
+                        <h4 class="card-title text-center mb-0">SIGN UP</h4>
                     </div>
                     <div class="card-body p-4">
                         <form action="dashboard/admin/authentication/admin-class.php" method="POST">
                             <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
+                            
+                            <div class="mb-3">
+                                <label for="username" class="form-label">Username</label>
+                                <input type="text" class="form-control" name="username" placeholder="Enter Username" required>
+                            </div>
                             
                             <div class="mb-3">
                                 <label for="email" class="form-label">Email</label>
@@ -38,24 +43,20 @@
                                 <label for="password" class="form-label">Password</label>
                                 <input type="password" class="form-control" name="password" placeholder="Enter Password" required>
                             </div>
-
-                            <div class="d-flex justify-content-end mb-3">
-                                <a href="forgot-password.php" class="text-decoration-none form-links">Forgot your password?</a>
-                            </div>
                             
                             <div class="d-grid mt-4">
-                                <button type="submit" class="btn btn-primary py-2" name="btn-signin">Sign In</button>
+                                <button type="submit" class="btn btn-primary" name="btn-signup">Sign Up</button>
                             </div>
                         </form>
                     </div>
-
                     <div class="card-footer bg-white py-3 text-center">
-                        <p class="text-decoration-none form-links">Don't have an account? <a class="text-decoration-none form-links" href="signup.php">Sign up here</a></p>
+                        <p class="text-decoration-none form-links">Already have an account? <a class="text-decoration-none form-links" href="index.php">Sign in here</a></p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
